d08670629704648de996ce0581ffb2bee081a10a
Max Vertlib <maximvert@gmail.com>
2021-03-10 12:19:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 2_000_000_000;
+        int refill = 500_000_000;
+        int total = account + refill;
+        System.out.println(total);
+
+    }
+}
